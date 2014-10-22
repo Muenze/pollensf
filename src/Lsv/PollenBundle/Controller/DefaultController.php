a385f06710ec9b734346e9e2dde1646fbc1509de
Guido Wehner <gwehner@Guidos-iMac.fritz.box>
2014-10-22 22:46:19 +0200

    stand

--- src/Lsv/PollenBundle/Controller/DefaultController.php
+++ src/Lsv/PollenBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace Lsv\PollenBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('LsvPollenBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function getAction(Request $request)
+    {
+        return new Response(json_encode(array('success' => true)), 200, array('Content-Type' => 'application/json'));
     }
 }
